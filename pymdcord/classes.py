534f8c04099dd4e5b7b29792cf2558460fa74c46
Shinwoo PARK <shinwoo.park@psw.kr>
2023-10-07 13:24:41 +0900

    refactor: dict to type strict class

---
+++ pymdcord/classes.py
@@ -0,0 +1,103 @@
+from dataclasses import dataclass, field
+from typing import Literal, Self
+
+INLINETYPE = Literal[
+    "inline",
+    "noclose",
+    "bold",
+    "underline",
+    "italic",
+    "strikethrough",
+    "code",
+    "codeblock",
+    "secret",
+]
+
+
+class b:
+    def dict(self):
+        return self.__dict__
+
+    def __repr__(self) -> str:
+        return str(self.dict())
+
+
+class c(b):
+    selftype: Literal[
+        "header",
+        "codeblock",
+        "list",
+        "blockquote",
+        "image",
+        "link",
+    ] | INLINETYPE
+
+    def dict(self):
+        return {**self.__dict__, "type": self.selftype}
+
+
+@dataclass
+class c_HEADER(c):
+    selftype = "header"
+    content: str
+    lv: int
+
+
+@dataclass
+class c_CODEBLOCK(c):
+    selftype = "codeblock"
+    lang: str | None
+    content: str
+
+
+@dataclass
+class c_LISTITEM(b):
+    lv: int
+    content: str
+
+
+@dataclass
+class c_LIST(c):
+    selftype = "list"
+    content: list[c_LISTITEM] = field(default_factory=list)
+
+
+@dataclass
+class c_BLOCKQUOTEITEM(b):
+    lv: int
+    content: str
+
+
+@dataclass
+class c_BLOCKQUOTE(c):
+    selftype = "blockquote"
+    content: list[c_BLOCKQUOTEITEM]
+
+
+@dataclass
+class c_IMAGE(c):
+    selftype = "image"
+    content: str | None
+    href: str
+
+
+@dataclass
+class c_LINKS(c):
+    selftype = "link"
+    content: str | None
+    href: str
+
+
+@dataclass
+class c_INLINECONTENT(c):
+    selftype: INLINETYPE
+    content: list[Self | c_LINKS | str] = field(default_factory=list)
+
+
+@dataclass
+class c_INLINE(c):
+    selftype = "inline"
+    content: list[c_INLINECONTENT | c_LINKS | str] = field(default_factory=list)
+
+
+ALLTYPE = c_HEADER | c_CODEBLOCK | c_BLOCKQUOTE | c_LIST | c_IMAGE | c_INLINE
--- pymdcord/main.py
+++ pymdcord/main.py
@@ -1,4 +1,24 @@
 import re
+from classes import (
+    ALLTYPE,
+    INLINETYPE,
+    c_HEADER,
+    c_CODEBLOCK,
+    c_BLOCKQUOTE,
+    c_BLOCKQUOTEITEM,
+    c_LIST,
+    c_LISTITEM,
+    c_IMAGE,
+    c_INLINE,
+    c_INLINECONTENT,
+    c_LINKS,
+)
+from typing import TypeVar
+from logging import getLogger, DEBUG, WARNING, StreamHandler
+
+logger = getLogger("pymdcord")
+logger.addHandler(StreamHandler())
+logger.setLevel(DEBUG)
 
 t_HEADER = r"^\s*(?P<lv>#{1,3})\s(?P<content>\s*[^\s]+.*\n?)$"
 t_LIST = r"^(?P<lv>\s*)([*\-+]\s|\d+\.\s)(?P<content>\s*[^\s]+.*\n?)$"
@@ -21,7 +41,7 @@ ILINK = re.compile(i_LINK)
 
 MASKIMAGE = re.compile(i_MASKIMAGE)
 
-INLINE_TRIGGERS = {
+INLINE_TRIGGERS: dict[INLINETYPE, list[str]] = {
     "bold": ["**"],
     "underline": ["__"],
     "italic": ["*", "_"],
@@ -35,22 +55,26 @@ INLINE_REGEXS = {
     "link": ILINK,
 }
 
+PARAGRAPH_EFFECT_PARSER_RETURN = TypeVar(
+    "PARAGRAPH_EFFECT_PARSER_RETURN", bound=c_INLINE | c_INLINECONTENT
+)
 
-def parse(t: str):
+
+def parse(t: str) -> list[ALLTYPE]:
     ind = 0
     p = t.splitlines(keepends=True)
-    res = []
+    res: list[ALLTYPE] = []
 
     def paragraph_effect_parser(
         _line: str,
-        reserved: list[tuple[int, int, str, None | str, str]],
-        # (span-start, span-end, type, content(None if non-mask link), href)
+        reserved: list[tuple[int, int, None | str, str]],
+        # (span-start, span-end, content(None if non-mask link), href)
         start_pos: int = 0,
-        triggered_by: str = None,
-        type_as: str = "inline",
-        root: bool = True,
-    ):
-        _res = {"type": type_as, "content": []}
+        triggered_by: str | None = None,
+        type_as: INLINETYPE = "inline",
+        root: PARAGRAPH_EFFECT_PARSER_RETURN = c_INLINE(),
+    ) -> tuple[int, PARAGRAPH_EFFECT_PARSER_RETURN]:
+        _res: PARAGRAPH_EFFECT_PARSER_RETURN = root
         _ind = start_pos
         while _ind <= len(_line) - 1:
             should_continue_without_bump = False
@@ -58,12 +82,8 @@ def parse(t: str):
                 for reserve in reserved:
                     if _ind == reserve[0]:
                         _ind = reserve[1]
-                        _res["content"].append(
-                            {
-                                "type": reserve[2],
-                                "content": reserve[3],
-                                "href": reserve[4],
-                            }
+                        _res.content.append(
+                            c_LINKS(content=reserve[2], href=reserve[3])
                         )
                         # TODO: link content inline parse later
                         should_continue_without_bump = True
@@ -72,52 +92,61 @@ def parse(t: str):
             for trigger_type, trigger in INLINE_TRIGGERS.items():
                 if (triggered := _line[_ind : _ind + len(trigger[0])]) in trigger:
                     if triggered_by in trigger:
-                        # print('closed by', triggered, _ind)  # debug
-                        # print((' ' if _ind - 1 < 0 else '') + line)  # debug
-                        # print(' '*(_ind - 1 if _ind - 1 >= 0 else _ind) + '^^')  # debug
+                        logger.debug(f"closed by {triggered} {_ind}")  # debug
+                        logger.debug((" " if _ind - 1 < 0 else "") + line)  # debug
+                        logger.debug(
+                            " " * (_ind - 1 if _ind - 1 >= 0 else _ind) + "^^"
+                        )  # debug
+                        _res.selftype = type_as
                         return _ind + len(triggered), _res
-                    # print('opened by', triggered, _ind)  # debug
-                    # print((' ' if _ind - 1 < 0 else '') + line)  # debug
-                    # print(' '*(_ind - 1 if _ind - 1 >= 0 else _ind) + '^^')  # debug
+                    logger.debug(f"opened by {triggered} {_ind}")  # debug
+                    logger.debug((" " if _ind - 1 < 0 else "") + line)  # debug
+                    logger.debug(
+                        " " * (_ind - 1 if _ind - 1 >= 0 else _ind) + "^^"
+                    )  # debug
                     _ind, _content_res = paragraph_effect_parser(
                         _line,
                         reserved,
                         _ind + len(triggered),
                         triggered,
                         trigger_type,
-                        False,
+                        c_INLINECONTENT("noclose", []),
                     )
-                    if _content_res["type"] == "noclose":
-                        if (
-                            len(_res["content"]) > 0
-                            and type(_res["content"][-1]) == str
-                        ):
-                            _res["content"][-1] += triggered
+                    if _content_res.selftype == "noclose":
+                        if len(_res.content) > 0 and type(_res.content[-1]) == str:
+                            _res.content[-1] += triggered  # type: ignore
                         else:
-                            _res["content"].append(triggered)
+                            _res.content.append(triggered)
+                        logger.debug(f"unclosed, rolling back to ( {_ind} )")  # debug
                     else:
-                        _res["content"].append(_content_res)
-                    # print('by closing this, we\'re here now! (', _ind, ')')  # debug
-                    # print((' ' if _ind - 1 < 0 else '') + line)  # debug
-                    # print(' '*(_ind - 1 if _ind - 1 >= 0 else _ind) + '^^')  # debug
+                        _res.content.append(_content_res)
+                        logger.debug(
+                            f"by closing this, we're here now! ( {_ind} )"
+                        )  # debug
+                    logger.debug((" " if _ind - 1 < 0 else "") + line)  # debug
+                    logger.debug(
+                        " " * (_ind - 1 if _ind - 1 >= 0 else _ind) + "^^"
+                    )  # debug
                     should_continue_without_bump = True
                     break
             if should_continue_without_bump:
                 continue
 
-            if len(_res["content"]) > 0 and type(_res["content"][-1]) == str:
-                _res["content"][-1] += _line[_ind]
+            if len(_res.content) > 0 and type(_res.content[-1]) == str:
+                _res.content[-1] += _line[_ind]  # type: ignore
             else:
-                _res["content"].append(_line[_ind])
+                _res.content.append(_line[_ind])
 
             _ind += 1
-        return start_pos, _res if root else {"type": "noclose", "content": []}
+        if triggered_by is None:
+            logger.debug("PARAGRAPH FINISH")
+        return start_pos, _res
 
     while ind < len(p):
         line = p[ind]
         ind += 1
         if codeblock_match := CODEBLOCK_START.fullmatch(line):
-            # print("CODEBLOCKSTART " + codeblock_match.group("lang"))
+            logger.debug("CODEBLOCKSTART " + codeblock_match.group("lang"))
             mem = ""
             while ind < len(p):
                 line = p[ind]
@@ -127,95 +156,95 @@ def parse(t: str):
                 mem += line
                 ind += 1
             res.append(
-                {
-                    "type": "codeblock",
-                    "lang": codeblock_match.group("lang"),
-                    "content": mem,
-                }
+                c_CODEBLOCK(
+                    lang=codeblock_match.group("lang"),
+                    content=mem,
+                )
             )
         elif header_match := HEADER.fullmatch(line):
-            # print("HEADER " + line[:-1])
+            logger.debug("HEADER " + line[:-1])
             res.append(
-                {
-                    "type": "header",
-                    "content": header_match.group("content"),
-                    "lv": len(header_match.group("lv")),
-                }
+                c_HEADER(
+                    content=header_match.group("content"),
+                    lv=len(header_match.group("lv")),
+                )
             )
         elif list_match := LIST.fullmatch(line):
-            # print("LISTSTART " + line[:-1])
+            logger.debug("LISTSTART " + line[:-1])
             mem = [
-                {
-                    "content": list_match.group("content"),
-                    "lv": len(list_match.group("lv")),
-                }
+                c_LISTITEM(
+                    lv=len(list_match.group("lv")), content=list_match.group("content")
+                )
             ]
             while ind < len(p):
                 line = p[ind]
                 if (flm := LIST.fullmatch(line)) or (
                     line != "\n" and line.strip()[0] not in ["*", "-", "+"]
                 ):
-                    # print("LISTITEM " + line[:-1])
                     if flm:
+                        logger.debug(
+                            "LISTITEM LV " + str(len(flm.group("lv"))) + " " + line[:-1]
+                        )
                         mem.append(
-                            {
-                                "content": flm.group("content"),
-                                "lv": len(flm.group("lv")),
-                            }
+                            c_LISTITEM(
+                                lv=len(flm.group("lv")), content=flm.group("content")
+                            )
                         )
                     else:
-                        # print("CONTINUED")
-                        mem[-1]["content"] += line
+                        logger.debug("CONTINUED LISTITEM " + line[:-1])
+                        mem[-1].content += line
                 elif line == "\n":
                     ind += 1
                     break
                 ind += 1
-            res.append({"type": "list", "content": mem})
+            res.append(c_LIST(content=mem))
         elif rlm := BLOCKQUOTE.fullmatch(line):
-            # print("BQUOTESTART " + line[:-1])
-            mem = [{"lv": len(rlm.group("lv")), "content": rlm.group("content")}]
+            logger.debug("BQUOTESTART " + line[:-1])
+            mem = [
+                c_BLOCKQUOTEITEM(lv=len(rlm.group("lv")), content=rlm.group("content"))
+            ]
             while ind < len(p):
                 line = p[ind]
                 if (flm := BLOCKQUOTE.fullmatch(line)) or (
                     line != "\n" and line.strip()[0] != ">"
                 ):
-                    # print("BQUOTEITEM " + line[:-1], end="")
                     if flm:
                         lv = len(flm.group("lv"))
-                        # print(" LV " + str(lv))
-                        mem.append({"lv": lv, "content": flm.group("content")})
+                        logger.debug("BQUOTE LV " + str(lv) + " " + line[:-1])
+                        mem.append(
+                            c_BLOCKQUOTEITEM(lv=lv, content=flm.group("content"))
+                        )
                     else:
-                        # print(" CONTINUED")
-                        mem[-1]["content"] += line
+                        logger.debug("CONTINUED BQUOTEITEM " + line[:-1])
+                        mem[-1].content += line
                 elif line == "\n":
                     ind += 1
                     break
                 ind += 1
-            res.append({"type": "blockquote", "content": mem})
+            res.append(c_BLOCKQUOTE(content=mem))
         elif image_match := MASKIMAGE.fullmatch(line):
+            logger.debug("MASKEDIMAGE " + line[:-1])
             res.append(
-                {
-                    "type": "image",
-                    "content": image_match.group("content"),
-                    "href": image_match.group("href"),
-                }
+                c_IMAGE(
+                    content=image_match.group("content"),
+                    href=image_match.group("href"),
+                )
             )
         else:
-            # print("IN PARAGRPAPH " + line[:-1])
+            logger.debug("IN PARAGRPAPH " + line[:-1])
             reserves = []
             for rematch in ILINK.finditer(line):
                 reserves.append(
                     (
                         rematch.start(),
                         rematch.end(),
-                        "link" if "barehref" in rematch.groupdict() else "masklink",
                         rematch.groupdict().get("content", None),
                         rematch.group("href")
                         if rematch.group("href") is not None
                         else rematch.group("barehref"),
                     )
                 )
-            _, _res = paragraph_effect_parser(line, reserves)
+            _, _res = paragraph_effect_parser(line, reserves, root=c_INLINE())
             res.append(_res)
     return res
 
