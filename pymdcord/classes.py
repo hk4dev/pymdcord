68d94beacb1832c536443e5bf70d35b253ea2ced
Shinwoo PARK <shinwoo.park@psw.kr>
2023-10-07 18:20:24 +0900

    fix: move escaped char and same quote as variable

--- pymdcord/classes.py
+++ pymdcord/classes.py
@@ -53,7 +53,8 @@ class c_CODEBLOCK(c):
     content: str
 
     def md(self) -> str:
-        return f"```{self.lang}\n{self.content.strip('\n')}\n```"
+        stripped = self.content.strip("\n")
+        return f"```{self.lang}\n{stripped}\n```"
 
 
 @dataclass
@@ -63,23 +64,26 @@ class c_LISTITEM(b):
     sortof: str
 
     def md(self) -> str:
-        return f"{' '*self.lv}{self.sortof}{('\n'+(' '*(self.lv+2))).join(self.content.strip("\n").split("\n"))}"
+        newlist = ("\n" + (" " * (self.lv + 2))).join(
+            self.content.strip("\n").split("\n")
+        )
+        return f"{' '*self.lv}{self.sortof}{newlist}"
 
 
 @dataclass
 class c_LIST(c):
     selftype = "list"
     content: list[c_LISTITEM] = field(default_factory=list)
-    
+
     def md(self) -> str:
-        return '\n'.join(map(lambda x: x.md(), self.content)) + "\n"
+        return "\n".join(map(lambda x: x.md(), self.content)) + "\n"
 
 
 @dataclass
 class c_BLOCKQUOTEITEM(b):
     lv: int
     content: str
-    
+
     def md(self) -> str:
         return ">" + ("\n> ".join(self.content.strip("\n").split("\n")))
 
@@ -88,10 +92,9 @@ class c_BLOCKQUOTEITEM(b):
 class c_BLOCKQUOTE(c):
     selftype = "blockquote"
     content: list[c_BLOCKQUOTEITEM]
-    
+
     def md(self) -> str:
         return "\n\n".join(list(map(lambda x: x.md(), self.content))) + "\n"
-        
 
 
 @dataclass
@@ -99,7 +102,7 @@ class c_IMAGE(c):
     selftype = "image"
     content: str | None
     href: str
-    
+
     def md(self) -> str:
         return self.href
 
@@ -109,19 +112,21 @@ class c_LINKS(c):
     selftype = "link"
     content: str | None
     href: str
-    
+
     def md(self) -> str:
         if self.content is None:
             return self.href
         else:
             return f"[{self.content}]({self.href})"
+
     ...
+
 
 @dataclass
 class c_INLINECONTENT(c):
     selftype: INLINETYPE
     content: list[Union["c_INLINECONTENT", c_LINKS, str]] = field(default_factory=list)
-    
+
     def md(self) -> str:
         mded = []
         for item in self.content:
@@ -129,7 +134,7 @@ class c_INLINECONTENT(c):
                 mded.append(item.md())
             else:
                 mded.append(item)
-        bald = ''.join(mded)
+        bald = "".join(mded)
         if self.selftype == "bold":
             return f"**{bald}**"
         elif self.selftype == "italic":
@@ -152,7 +157,7 @@ class c_INLINECONTENT(c):
 class c_INLINE(c):
     selftype = "inline"
     content: list[c_INLINECONTENT | c_LINKS | str] = field(default_factory=list)
-    
+
     def md(self) -> str:
         mded = []
         for item in self.content:
@@ -160,7 +165,7 @@ class c_INLINE(c):
                 mded.append(item.md())
             else:
                 mded.append(item)
-        return ''.join(mded)
+        return "".join(mded)
 
 
 ALLTYPE = c_HEADER | c_CODEBLOCK | c_BLOCKQUOTE | c_LIST | c_IMAGE | c_INLINE
